820128d7d87a10401de8d8018c78ed810a521541
Kiruthika M <kiruthika.shalinimanivannan@gmail.com>
2025-10-28 21:17:59 +0530

    Basic imports
    Basic import statements and YOLOV8 Model

---
+++ main.py
@@ -0,0 +1,17 @@
+# Core Libraries
+import os
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+# YOLOv8 Model
+from ultralytics import YOLO
+
+# Torch & Utilities
+import torch
+import torchvision
+
+# Optional Visualization (can comment out if not used)
+import seaborn as sns
